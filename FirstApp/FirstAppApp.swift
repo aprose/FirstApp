035980609ed76e2e28611f6a8ebf5a71a684d260
Adrian Rose <apsrose@hotmail.com>
2021-06-18 19:05:28 +0200

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Adrian Rose on 18.06.21.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
